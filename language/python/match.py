f1ce9665077f49f04c02569a1267c6c042b65e75
EunseoPark <88071251+arty0928@users.noreply.github.com>
2023-03-19 23:13:57 +0900

    "2-자료형"

---
+++ language/python/match.py
@@ -0,0 +1,14 @@
+class Point:
+    x: int
+    y: int
+
+def where_is(point):
+    match point:
+        case Point(x=0, y=0):
+            print("Origin")
+        case Point(x=0, y=y):
+            print(f"Y={y}")
+        case Point(x=x, y=0):
+            print(f"X={x}")
+
+Point(x=1, y=0)
--- language/python/python1.py
+++ language/python/python1.py
@@ -1,1 +1,13 @@
-print('hello world');
+
+word = 'Python';
+print(word[2:5]);
+
+squares = [1,4,5,6,7];
+print(squares);
+squares.append([9,9]);
+print(squares);
+squares[2:5]=[];
+print(len(squares));
+print(squares);
+squares[:]=[];
+print(squares);
