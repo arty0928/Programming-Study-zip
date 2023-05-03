d2bdf40c100bb445a1f74b66a7c7081d878a5028
EunseoPark <88071251+arty0928@users.noreply.github.com>
2023-05-03 23:54:50 +0900

    1. 그리디 : 큰수의 법칙 mine clear

--- algorithm/1. 그리디/1.2 큰 수의 법칙.py
+++ algorithm/1. 그리디/1.2 큰 수의 법칙.py
@@ -1,27 +1,20 @@
 from operator import length_hint
-from pstats import SortKey
-
 
 N,M,K = map(int, input().split())
-print(N,M,K)
 list1 = list(map(int, input().split()))
 list2 = [0]*length_hint(list1)
 list1.sort(reverse=True)
-
 result = 0
 
 for i in range(M):
-    print("range",i,"===========")
-    for j in list2:
-        if(list2[j]==3):
-            print("list2",j,"=",list2[j])
-            list2[j]==0
-            break
-        elif(list2[j]<K):
-            print("result +=list2","[",j,"] =",list2[j])
-            result += list1[j]
-            print("result",result)
+    for j in range(len(list2)):
+        if(list2[j]< K):
+            print("result +=list2","[",j,"] =",list2[j], list1[j])
+            result +=list1[j]
             list2[j] +=1
             break
+        elif(list2[j] == 3):
+            print(j, list2[j])
+            list2[j]=0
 
 print(result)
