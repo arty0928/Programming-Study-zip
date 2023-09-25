fab33be8e3cec15b593a4cd125250e9d5005516e
EunseoPark <88071251+arty0928@users.noreply.github.com>
2023-09-25 22:12:46 +0900

    3. quick sort

--- algorithm/6. 정렬/2. 삽입정렬.py
+++ algorithm/6. 정렬/2. 삽입정렬.py
@@ -1,13 +1,19 @@
 # 특정 데이터를 적절한 위치에 삽입 
 # 적절한 위치 앞까지는 이미 정렬 완료
 
+# 시간 복잡도 : O(N) ~ O(N의 제곱) 
+# 이미 거의 정렬된 경우에는 O(N)로 퀵 정렬보다 빠르다
+
 array = [7,8,4,7,2,1,6,4,3,0]
 
-for i in range(1, len(array)):
-    for j in range(len(array)):
-        v = array.pop(array[i])
-        if(array[j]<v and v<array[j+1]):
-            array.insert(j,v)
-    array.insert(j,v)
+for i in range(len(array)):
+    # i부터 1까지 -1씩 감소
+    for j in range(i,0,-1):
+        if array[j]<array[j-1]:
+            array[j],array[j-1] = array[j-1],array[j]
+        
+        # 자기보다 크거나 같으면 멈춤
+        else:
+            break
 
 print(array)
---
+++ algorithm/6. 정렬/3. 퀵 정렬.py
@@ -0,0 +1,38 @@
+array = [5,7,9,0,3,1,6,2,4,8]
+
+def quick(array,start,end):
+    
+    # array에 하나만 남아있다면
+    if start >=end:
+        return
+    
+    pivot = start
+    left = start + 1
+    right = end    
+    
+    while left<=right:
+    
+        # pivot보다 큰 숫자가 나올때까지 left + 1
+        while left <= end and array[pivot] >= array[left]:
+            left +=1
+        
+        # pivot보다 작은 숫자가 나올때까지 right -1
+        # start <= right되면 한개만 남았을때 right-1하면 범위를 벗어나게 됨
+        while start < right and array[pivot] <= array[right]:
+            right -=1
+        
+        # 엇갈리면
+        if left>right:
+            array[right],array[pivot] = array[pivot],array[right]
+        
+        # 엇갈리지 않았다면 작은 데이터(right), 큰 데이터(left)
+        else:
+            array[left],array[right] = array[right],array[left]
+    
+    # pivot이 맨 앞에 있는 원소이니까, 제일 뒤에 있는 원소는 right
+    quick(array,start,right-1)
+    quick(array,right+1,end)
+
+quick(array, 0, len(array)-1)
+print(array)
+        
